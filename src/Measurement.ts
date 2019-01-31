6b35240850a2f18dddfaafa1440781ed6ec8e1ce
Dan Deboer <daniel.s.deboer@gmail.com>
2019-01-31 15:38:50 -0500

    Simplify build process

--- src/Measurement.ts
+++ src/Measurement.ts
@@ -2,28 +2,43 @@ import { PrecisionInterface as IPrecision } from '@deboertool/vo-precision'
 import { IUnit } from '@deboertool/vo-unit'
 import IMeasurement from './Interfaces/IMeasurement'
 
-export default class Measurement implements IMeasurement  {
+export default class Measurement implements IMeasurement  
+{
   constructor (
     protected _value: number, 
     protected _precision: IPrecision,
-    protected _units: IUnit
+    protected _unit: IUnit
   ) {}
 
-  value (): number {
+  value (): number 
+  {
     return this._value
   }
 
-  clone (value: number): IMeasurement {
-    return new Measurement(value, this._precision, this._units)
+  precision (): IPrecision
+  {
+    return this._precision
   }
 
-  times (multiplier: number): IMeasurement {
+  unit (): IUnit
+  {
+    return this._unit
+  }
+
+  clone (value: number): IMeasurement 
+  {
+    return new Measurement(value, this._precision, this._unit)
+  }
+
+  times (multiplier: number): IMeasurement 
+  {
     return this.clone(
       this.value() * multiplier
     )
   }
 
-  minus (minuend: number): IMeasurement {
+  minus (minuend: number): IMeasurement 
+  {
     return this.clone(
       this.value() - minuend
     )
