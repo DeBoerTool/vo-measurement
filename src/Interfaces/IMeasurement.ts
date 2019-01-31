36a69a075443e43758cd10c589fd1700f00b0882
Dan Deboer <daniel.s.deboer@gmail.com>
2019-01-31 15:55:50 -0500

    Add getters to interface

--- src/Interfaces/IMeasurement.ts
+++ src/Interfaces/IMeasurement.ts
@@ -1,6 +1,10 @@
 import ValueObject from '@deboertool/value-object-interface'
+import { PrecisionInterface as IPrecision } from '@deboertool/vo-precision'
+import { IUnit } from '@deboertool/vo-unit'
 
 interface IMeasurement extends ValueObject<number> {
+  precision (): IPrecision
+  unit (): IUnit
   clone (value: number): IMeasurement
   times (value: number): IMeasurement
   minus (value: number): IMeasurement
