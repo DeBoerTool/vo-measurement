395091957e31ac447d49a51a3163c9b19398a5c0
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2019-02-01 11:53:36 -0500

    Update index

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import Measurement from './src/Measurement'
-import IMeasurement from './src/Interfaces/IMeasurement'
+import IMeasurement from './src/IMeasurement'
 
 export { Measurement, IMeasurement }
 export default Measurement
